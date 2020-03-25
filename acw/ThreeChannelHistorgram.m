59aad07a27517c85b755c707af3ffb7d8d09a368
James Gordon <james.gordon1628@gmail.com>
2020-03-25 22:40:02 +0000

    Added single channel convolve, and added S+P noise identification

--- acw/ThreeChannelHistorgram.m
+++ acw/ThreeChannelHistorgram.m
@@ -4,15 +4,15 @@ subplot(2,2,1);
 imshow(img);
 title("Original Image");
 subplot(2,2,2);
-r = histogram(img(:,:,1)).Data;
+r = histogram(img(:,:,1)).BinCounts;
 histogram(img(:,:,1));
 title("Channel One");
 subplot(2,2,3);
-g = histogram(img(:,:,2)).Data;
+g = histogram(img(:,:,2)).BinCounts;
 histogram(img(:,:,2));
 title("Channel Two");
 subplot(2,2,4);
-b = histogram(img(:,:,3)).Data;
+b = histogram(img(:,:,3)).BinCounts;
 histogram(img(:,:,3));
 title("Channel Three");
 
